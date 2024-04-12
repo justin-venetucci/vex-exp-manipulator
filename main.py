3c8a0a0304ed8d39666fe40ea6df2ba707399e9d
Justin Venetucci <40038846+justin-venetucci@users.noreply.github.com>
2024-04-12 10:35:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ encoder_bicep = Encoder(brain.three_wire_port.c)
 ptmr = Potentiometer(brain.three_wire_port.h)
 elbow = Motor(Ports.PORT5, False)
 claw = Servo(brain.three_wire_port.b)
+dist = Distance(Ports.PORT6)
 
 
 # Wait for sensor(s) to fully initialize
@@ -73,15 +74,13 @@ def forwardKin(shoulderA, elbowA):
 #which direction is theta increasing?
 #where are you measuring your angle 0 from
 
-    # forward kinematics
-
     alpha = math.pi - shoulderA - elbowA
     alpha = math.radians(alpha)
 
     x2 = x - forearm_length * math.cos(alpha)
     y2 = y + forearm_length * math.sin(alpha)
 
-    tipLocation = (x2,y2)
+    tipLocation = (x2, y2)
 
     print("tip: " + str(tipLocation))
     return tipLocation
@@ -94,6 +93,7 @@ def goto(shoulderA, elbowA, skipSafety):
     # stop motors
     shoulder.stop()
     elbow.stop()
+
 
     # project tip location with forward kinematics
     projTipLocation = forwardKin(shoulderA, elbowA)
@@ -110,20 +110,43 @@ def goto(shoulderA, elbowA, skipSafety):
     else:
         # angles are safe to go to
         print("GoTo validated")
+        
+        encoder_moved = False
+
+        # bring shoulder up to angle
         while encoder_bicep.position(DEGREES) < shoulderA:
             shoulder.spin(REVERSE)
             wait(0.1,SECONDS)
-            print("enc angle: ", encoder_bicep.position(DEGREES))
+            print("enc angle up: ", encoder_bicep.position(DEGREES))
+            encoder_moved = True
+
+        # bring shoulder down to angle
+        while encoder_bicep.position(DEGREES) > shoulderA and encoder_moved != True:
+            shoulder.spin(FORWARD)
+            wait(0.1,SECONDS)
+            print("enc angle dn: ", encoder_bicep.position(DEGREES))
+        
         shoulder.stop()
 
-        while ptmr.angle(DEGREES) + ptmr_offset > elbowA:
+        elbow_moved = False
+
+        # bring elbow down to angle
+        while 180 - ptmr.angle(DEGREES) + ptmr_offset < elbowA:
             elbow.spin(REVERSE)
             wait(0.1,SECONDS)
-            print("ptmr angle: ", ptmr.angle(DEGREES) + ptmr_offset )
+            print("ptmr angle dn: ", 180 - ptmr.angle(DEGREES) + ptmr_offset )
+            elbow_moved = True
+
+        # bring elbow up to angle
+        while 180 - ptmr.angle(DEGREES) + ptmr_offset > elbowA and elbow_moved != True:
+            elbow.spin(FORWARD)
+            wait(0.1,SECONDS)
+            print("ptmr angle up: ", 180 - ptmr.angle(DEGREES) + ptmr_offset )
         elbow.stop()    
 
 
 def invKin(x, y):
+
     print("invKin called with", x, ",", y)
     #given a desired coordinate pair, calculates the angles
     #necessary to send the claw to that location
@@ -131,51 +154,89 @@ def invKin(x, y):
     #side lengths a and b are constant    
     l1 = bicep_length
     l2 = forearm_length
+    r = math.sqrt((x ** 2) + (y ** 2))
 
     ##Solve for theta2
     cos_theta2 = (x**2 + y**2 - l1**2 - l2**2) / (2 * l1 * l2)
     theta2 = math.acos(cos_theta2)
-    theta2 = math.degrees(theta2)
+    
 
     ##Solve for theta1
     alpha = math.atan(y / x)
+    s = math.asin((9.5 * math.sin(theta2)) / r)
+    theta1 = alpha + s
 
-    theta1_numerator = l2  * math.sin(theta2)
-    theta1_denominator = math.sqrt(x**2 + y**2)
-    theta1_fraction = theta1_numerator / theta1_denominator
-    theta1 = alpha - math.asin( theta1_fraction )
 
     theta1 = math.degrees(theta1)
-
+    theta2 = math.degrees(theta2)
     print("theta1: ", theta1)
     print("theta2: ", theta2)
 
-    angles = (theta1, theta2)
+    angles = (theta1 - 1, theta2 - 6)
     return angles
+
+
+def grab(x, y):
+
+    claw.set_position(100, DEGREES) # open claw
+
+    angles = invKin(x+1, y)
+
+    goto(angles[0], angles[1], True)
+
+    claw.set_position(-50, DEGREES) # close claw
+
+    wait(1, SECONDS)
+    
+    # move elbow to top
+    goto(25,1,True)
+    elbow.spin(FORWARD)
+    print("free spin forward")
+    wait(1, SECONDS)
+    elbow.stop()
+
+    # throw can back over the top
+    goto(90,1,True)
+    wait(3,SECONDS)
+    claw.set_position(100, DEGREES) # open claw
+
+    # go back to a neutral position
+    #goto(encoder_bicep.position(DEGREES), 40, True)
+    elbow.spin(REVERSE)
+    print("free spin back")
+    wait(2, SECONDS)
+    elbow.stop()
+    goto(25,45,True)
+
 
 #--------------------------------------------------
 #MAIN
 #--------------------------------------------------
 
 # set constants
-bicep_length = 9.75
-forearm_length = 10.55
-ptmr_offset = -30 #deg
+bicep_length = 10
+forearm_length = 9.5
+ptmr_offset = 20 #deg
 wait(0.1,SECONDS)
 
-#claw.set_position(-50,DEGREES) open
+claw.set_position(100,DEGREES) #open
 #wait(1, SECONDS)
-#claw.set_position(100,DEGREES) close
+#claw.set_position(-50,DEGREES) close
 
+# calibrate the encoder
 Calibrate()
 
-#safe test
-#goto(60, 90, True)
-#wait(10, SECONDS)
+# go to a neutral position
+goto(25,40,True)
 
-anglesTuple = invKin(15, 10)
-goto(anglesTuple[0],anglesTuple[1] + 60, True)
-#goto(68, 54, True)
+while(dist.object_distance(INCHES) < 60):
+    print("------------ Place can ------------")
+    brain.play_note(1,2,250)    
+    wait(5, SECONDS)
+    brain.play_note(1,4,250)
+    print("can location: ", dist.object_distance(INCHES) + 4, " ", 0)
+    grab(dist.object_distance(INCHES) + 4, -3)
+
 
 #--------------------------------------------------
     
